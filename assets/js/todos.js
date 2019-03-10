0832b72760290c9ba272cc4ffc146b0ed6824073
Szilvi Horvath <szil.horvath@gmail.com>
2019-03-10 19:19:32 +0000

    add functionality to add new todo items

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,21 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 //click on X to delete todo item
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   event.stopPropagation();
 })
+
+//add new todo items from input
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span> X </span>" + todoText + "</li>")
+  };
+});
